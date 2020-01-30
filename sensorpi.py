d13064a6b875e634916f507b9311a73cd07ff044
Daniel Kofler <fox27374@gmail.com>
2020-01-30 15:10:46 +0000

    Initial

--- sensorpi.py
+++ sensorpi.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 
-
 import os
 import time
 import json
+import requests
+from datetime import datetime
 from scapy.all import *
 
 # Configuration
 scanTime = 10
-channels = (1, 6, 11)
 
 # Variables
 wlans = {}
+pkts = []
 
 if len(sys.argv) ==  2:
     iface = str(sys.argv[1])
@@ -22,6 +23,13 @@ os.system("ifconfig " + iface + " down")
 os.system("iwconfig " + iface + " mode monitor")
 os.system("ifconfig " + iface + " up")
 
+
+def readConfig(configFile):
+    with open(configFile, 'r') as cf:
+        configDict = json.load(cf)
+    return configDict
+
+
 def beacon(pkt):
     if pkt.haslayer(Dot11Beacon):
         ap = pkt.addr2
@@ -30,6 +38,48 @@ def beacon(pkt):
         channel = int(ord(pkt[Dot11Elt:3].info))
         wlan = {'SSID':ssid.decode('UTF-8'), 'AP':ap, 'BSSID':bssid, 'CHANNEL':channel}
         createWlanList(wlan)
+
+def data(pkt):
+    pktRetry = '0'
+    if 'retry' in pkt.FCfield:
+        pktRetry = '1'
+    if pkt.type == 0: pktType = 'Management'
+    if pkt.type == 1: pktType = 'Control'
+    if pkt.type == 2: pktType = 'Data'
+
+    pktBSSID = pkt[Dot11].addr3
+    pktSubtype = pkt.subtype
+    pktTime = datetime.now().isoformat()
+    #pktChannel = int(ord(pkt[Dot11Elt:3].info))
+    pktChannel = pkt[RadioTap].Channel
+    if pktType == 0:
+        pktSSID = pkt[Dot11Elt].info
+        pktSSID = pktSSID.decode('UTF-8')
+    else:
+        pktSSID = 'NA'
+
+    pktInfo = {"event":{"time":str(pktTime), "type":pktType, "subtype":pktSubtype, "ssid":pktSSID, "bssid":pktBSSID, "channel":pktChannel, "flags":{"retry":pktRetry}}}
+
+    #print(pktTime)
+    aggregateData(pktInfo)
+    #print(json.dumps(pktInfo, indent=4))
+    #sendData('172.24.89.171', 'b6b02802-eaf6-4d17-89b1-0fd6d66d2673', pktInfo)
+
+
+def aggregateData(pktInfo):
+    global pkts
+    if len(pkts) <= 100:
+        pkts.append(pktInfo)
+        #print(pkts)
+    else:
+        sendData(pkts)
+        pkts = []
+        pkts.append(pktInfo)
+
+def sendData(pkts):
+    url = 'https://' + splunkServer + ':' + splunkPort + splunkURL
+    authHeader = {'Authorization': 'Splunk %s'%splunkToken}
+    req = requests.post(url, headers=authHeader, json=pkts, verify=False)
 
 def createWlanList(wlan):
     ssid = wlan['SSID']
@@ -46,18 +96,31 @@ def createWlanList(wlan):
         if addValue == True:
             wlans[ssid].append(wlanValue)
 
-    # Create new SSID
+    # Add new SSID
     else:
         valueList = []
         valueList.append(wlanValue)
         wlans[ssid] = valueList
 
+# Configuration
+sensorPiConfig = readConfig('config.json')
+channels = sensorPiConfig['SensorPi']['Channels']
+splunkServer = sensorPiConfig['Splunk']['Server']
+splunkPort = sensorPiConfig['Splunk']['Port']
+splunkURL = sensorPiConfig['Splunk']['URL']
+splunkToken = sensorPiConfig['Splunk']['Token']
+
 
 print('Scanning for %s seconds to get all WLANs on channels %s'%(scanTime, channels))
 loopTime = time.time() + scanTime
-while time.time() < loopTime:
-    for channel in channels:
-        os.system("iwconfig " + iface + " channel " + str(channel))
-        sniff(iface=iface, prn=beacon, count=10, timeout=3, store=0)
+#while time.time() < loopTime:
 
-print(json.dumps(wlans, indent=4))
+#os.system("iwconfig " + iface + " channel " + str(channel))
+os.system("iwconfig " + iface + " channel " + str(1))
+while 1:
+    #for channel in channels:
+    #sniff(iface=iface, prn=beacon, count=10, timeout=3, store=0)
+    sniff(iface=iface, prn=data, count=10, timeout=3, store=0)
+
+#sendData(json.dumps(wlans))
+#print(json.dumps(wlans, indent=4))
