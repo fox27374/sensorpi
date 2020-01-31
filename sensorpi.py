300fc6362b1a639bfa843d103c76f9a08e1981d6
Daniel Kofler <fox27374@gmail.com>
2020-01-31 12:42:12 +0000

    additional features

--- sensorpi.py
+++ sensorpi.py
@@ -18,28 +18,13 @@ def changeIfaceMode(iface):
     os.system("iwconfig " + iface + " mode monitor")
     os.system("ifconfig " + iface + " up") 
 
-def beacon(pkt):
-    if pkt.haslayer(Dot11Beacon):
-        ap = pkt.addr2
-        ssid = pkt[Dot11Elt].info
-        bssid = pkt[Dot11].addr3    
-        channel = int(ord(pkt[Dot11Elt:3].info))
-        wlan = {'SSID':ssid.decode('UTF-8'), 'AP':ap, 'BSSID':bssid, 'CHANNEL':channel}
-        createWlanList(wlan)
-
 def data(pkt):
     global frameTypes
+    global scanWLANBSSIDs
     pktRetry = False
     pktToDS = False
     pktFromDS = False
-
     pktBSSID = pkt[Dot11].addr3
-    pktRetry = pkt[Dot11].FCfield.retry != 0
-    pktType = frameTypes[str(pkt.type)]['Name']
-    pktSubtype = frameTypes[str(pkt.type)][str(pkt.subtype)]
-    pktTime = datetime.now().isoformat()
-
-
     # Get ToDS and FromDS flags if packet is of type data
     # and read BSSID from a different address field
     if pkt.type == 2:
@@ -47,22 +32,29 @@ def data(pkt):
         pktFromDS = pkt.FCfield & 0x5 != 0
         if pktToDS: pktBSSID = pkt[Dot11].addr1
         if pktFromDS: pktBSSID = pkt[Dot11].addr2
+    # Check if BSSID is in one of the addres fields
+    # otherwise the frame is not interresting
+    if pktBSSID in scanWLANBSSIDs:
+        pktRetry = pkt[Dot11].FCfield.retry != 0
+        pktType = frameTypes[str(pkt.type)]['Name']
+        pktSubtype = frameTypes[str(pkt.type)][str(pkt.subtype)]
+        pktTime = datetime.now().isoformat()
 
-    #pktChannel = int(ord(pkt[Dot11Elt:3].info))
-    pktChannel = pkt[RadioTap].Channel
-    if pktType == 0:
-        pktSSID = pkt[Dot11Elt].info
-        pktSSID = pktSSID.decode('UTF-8')
-    else:
-        pktSSID = 'NA'
+        #pktChannel = int(ord(pkt[Dot11Elt:3].info))
+        pktChannel = pkt[RadioTap].Channel
+        if pktType == 0:
+            pktSSID = pkt[Dot11Elt].info
+            pktSSID = pktSSID.decode('UTF-8')
+        else:
+            pktSSID = 'NA'
 
-    pktInfo = {"event":{"time":str(pktTime), "type":pktType, "subtype":pktSubtype, "tods":pktToDS, "fromds":pktFromDS, "ssid":pktSSID, "bssid":pktBSSID, "channel":pktChannel, "retry":pktRetry}}
-    aggregateData(pktInfo)
+        pktInfo = {"event":{"time":str(pktTime), "type":pktType, "subtype":pktSubtype, "tods":pktToDS, "fromds":pktFromDS, "ssid":pktSSID, "bssid":pktBSSID, "channel":pktChannel, "retry":pktRetry}}
+        aggregateData(pktInfo)
 
 
 def aggregateData(pktInfo):
     global pkts
-    if len(pkts) <= 100:
+    if len(pkts) <= int(splunkBulk):
         pkts.append(pktInfo)
     else:
         sendData(pkts)
@@ -74,26 +66,6 @@ def sendData(pkts):
     authHeader = {'Authorization': 'Splunk %s'%splunkToken}
     req = requests.post(url, headers=authHeader, json=pkts, verify=False)
 
-def createWlanList(wlan):
-    ssid = wlan['SSID']
-    ap = wlan['AP']
-    channel = wlan['CHANNEL']
-    wlanValue = {'AP':ap, 'CHANNEL':channel}
-    addValue = True
-    
-    # Append values if SSID exists (no duplicates)
-    if ssid in wlans.keys():
-        for a in wlans[ssid]:
-            if a['AP'] == ap:
-                addValue = False
-        if addValue == True:
-            wlans[ssid].append(wlanValue)
-
-    # Add new SSID
-    else:
-        valueList = []
-        valueList.append(wlanValue)
-        wlans[ssid] = valueList
 
 # Load Configuration
 configFile = 'config.json'
@@ -106,6 +78,7 @@ frameTypes = readConfig(frameTypesFile)
 iface = sensorPiConfig['SensorPi']['Interface']
 channels = sensorPiConfig['SensorPi']['Channels']
 scanTime = sensorPiConfig['SensorPi']['Scantime']
+channelTime = sensorPiConfig['SensorPi']['Channeltime']
 wlansFile = sensorPiConfig['SensorPi']['wlansFile']
 frameTypesFile = sensorPiConfig['SensorPi']['frameTypesFile']
 # Splunk
@@ -113,21 +86,37 @@ splunkServer = sensorPiConfig['Splunk']['Server']
 splunkPort = sensorPiConfig['Splunk']['Port']
 splunkURL = sensorPiConfig['Splunk']['URL']
 splunkToken = sensorPiConfig['Splunk']['Token']
+splunkBulk = sensorPiConfig['Splunk']['Bulk']
 
 # Variables
-wlans = {}
+scanWLAN = 'Aironet-NTS-Secure'
 pkts = []
-
-# System preparation
-changeIfaceMode(iface)            
             
-print('Scanning for %s seconds to get all WLANs on channels %s'%(scanTime, channels))
-loopTime = time.time() + scanTime
-#while time.time() < loopTime:
+# Read WLANs from file
+wlans = readConfig(wlansFile)
 
-os.system("iwconfig " + iface + " channel " + str(channel))
-while 1:
-    #for channel in channels:
-    #sniff(iface=iface, prn=beacon, count=10, timeout=3, store=0)
-    sniff(iface=iface, prn=data, count=10, timeout=3, store=0)
+# Check if requested WLAN is stored already
+if scanWLAN in wlans.keys():
+    # System preparation
+    print('Setting interface %s to monitor mode' %iface)
+    changeIfaceMode(iface)            
+    scanWLANBSSIDs = []
+    scanWLANChannels = []
+    for wlanInfo in wlans[scanWLAN]:
+        scanWLANBSSIDs.append(wlanInfo['bssid'])
+        scanWLANChannels.append(wlanInfo['channel'])
+
+    print('Start scanning on channels ' + str(scanWLANChannels) + ' for WLAN ' + scanWLAN)
+    print('Scanning every channel for ' + channelTime + ' seconds')
+    print('Sending data to Splunk in bulks of %s' %splunkBulk)
+
+    while 1:
+        for scanWLANChannel in scanWLANChannels:
+            os.system("iwconfig " + iface + " channel " + str(scanWLANChannel))
+            loopTime = time.time() + int(channelTime)
+            while time.time() < loopTime:
+                sniff(iface=iface, prn=data, count=10, timeout=3, store=0)
+
+else:
+    print('WLAN %s not found in list, please scan first.' %scanWLAN)
 
