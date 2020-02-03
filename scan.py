2ec661ccbd7eb0a940be4fef0e729de3a055c700
Daniel Kofler <fox27374@gmail.com>
2020-02-03 17:01:54 +0000

    Added frequencies table

--- scan.py
+++ scan.py
@@ -10,7 +10,8 @@ def beacon(pkt):
     if pkt.haslayer(Dot11Beacon):
         ssid = pkt[Dot11Elt].info
         bssid = pkt[Dot11].addr3    
-        channel = int(ord(pkt[Dot11Elt:3].info))
+        #channel = int(ord(pkt[Dot11Elt:3].info))
+        channel = pkt[RadioTap].Channel
         wlan = {'ssid':ssid.decode('UTF-8'), 'bssid':bssid, 'channel':channel}
         createWlanList(wlan, wlans)
 
@@ -20,12 +21,13 @@ sensorPiConfig = readConfig(configFile)
 
 # SensorPi
 iface = sensorPiConfig['SensorPi']['Interface']
-channels = sensorPiConfig['SensorPi']['Channels']
+#channels = sensorPiConfig['SensorPi']['Channels']
 scanTime = sensorPiConfig['SensorPi']['Scantime']
 wlansFile = sensorPiConfig['SensorPi']['wlansFile']
 
 # Variables
 wlans = {}
+channels= [1,6,11,36,40,44,48]
 
 # System preparation
 changeIfaceMode(iface)            
